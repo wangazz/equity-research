2c54c8b88a76e718dbcf65c47abd73ff120d2ee4
Andy Wang <andywang.16@gmail.com>
2020-01-11 14:40:10 +1000

    Initial commit of main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+
+ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+data['4. close'].plot()
+plt.title('Intraday Times Series for the MSFT stock (1 min)')
+plt.show()
